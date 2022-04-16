df85a95630810dfa0e3eb38edb9427e3a108d133
RenanTelini <renan_telini@hotmail.com>
2022-04-15 21:44:02 -0300

    341. Subtotal & Total methods

--- src/main/java/com/store/entities/Payment.java
+++ src/main/java/com/store/entities/Payment.java
@@ -12,6 +12,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
@@ -21,16 +23,16 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	
+
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
-	
+
 	public Payment() {
-		
+
 	}
- 
-	 
+
 	public Payment(Long id, Instant moment, Order order) {
 		super();
 		this.id = id;
@@ -78,6 +80,5 @@ public class Payment implements Serializable {
 		Payment other = (Payment) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
